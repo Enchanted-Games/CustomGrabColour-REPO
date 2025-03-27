8cd056817a010ae5bfcac2e71eaa405c720fbba0
ioblackshaw <58823778+Enchanted-Games@users.noreply.github.com>
2025-03-27 22:01:51 +0000

    initial

---
+++ CustomGrabColour/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+
+using BepInEx;
+
+using HarmonyLib;
+using BepInEx.Logging;
+
+namespace CustomGrabColour
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_ID = "CustomGrabColour";
+        public const string PLUGIN_NAME = "CustomGrabColour";
+        public const string PLUGIN_VERSION = "0.0.1";
+        public const string PLUGIN_GUID = "games.enchanted.CustomGrabColour";
+    }
+
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static Plugin Instance { get; private set; }
+
+        public ManualLogSource PluginLogger;
+
+        private void Awake()
+        {
+            Instance = this;
+
+            PluginLogger = Logger;
+
+            // Apply Harmony patches (if any exist)
+            Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            harmony.PatchAll();
+
+            // Plugin startup logic
+            PluginLogger.LogInfo($"omg the mod is loading! {PluginInfo.PLUGIN_NAME} ({PluginInfo.PLUGIN_GUID})");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# MoreSoundboardSounds
+Adds a few more sounds to the meme soundboard. [MemeSoundboard](https://thunderstore.io/c/lethal-company/p/Flof/MemeSoundboard/) is required!
+
+## Added sounds
+Currently this mod adds the following sounds:
+```
+- GET OUT
+- theres an impostor among us
+- AHHH HAELP HAELP HAELP
+- WAIT WAIT WAIT
+- keydrop.mp3
+- although for one of you...
+- skidaddle
+- YOU'LL NEVER SEE IT COMING
+- to be continued...
+- i just lost my dawwwwg
+- THIS IS A MOMENT IN HISTORY
+```
