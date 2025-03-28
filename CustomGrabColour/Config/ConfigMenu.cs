4edff139d422ea6067e576a19d87b668179e4968
ioblackshaw <58823778+Enchanted-Games@users.noreply.github.com>
2025-03-28 19:48:35 +0000

    get a rectangle to draw in config screen

--- CustomGrabColour/Config/ConfigMenu.cs
+++ CustomGrabColour/Config/ConfigMenu.cs
@@ -1,70 +1,92 @@
 using CustomGrabColour;
 using MenuLib;
 using MenuLib.MonoBehaviors;
-using MenuLib.Structs;
 using System;
 using UnityEngine;
-using UnityEngine.UIElements;
-using static UnityEngine.ParticleSystem.PlaybackState;
+using UnityEngine.UI;
 
 class ConfigMenu
 {
+    private static Image neutralGrabColourPreviewImage;
+    private static GameObject neutralGrabColourPreviewParentObject;
+
     public static void Init()
     {
         MenuAPI.AddElementToEscapeMenu(parent =>
         {
-            //int amountOfChildren = parent.GetChildCount();
-            //for (int i = 0; i < amountOfChildren; i++)
-            //{
-            //    Plugin.LogMessage("Child [" + i + "]: " + parent.GetChild(i).name);
-            //}
-            var repoButton = MenuAPI.CreateREPOButton("Change Grab Colour", () => BuildAndOpenPopup(), parent, localPosition: new Vector2(28.3f, 350.0f));
+            // add button to open colour config screen
+            var repoButton = MenuAPI.CreateREPOButton("Change Grab Colour", () => OpenPopup(), parent, localPosition: new Vector2(28.3f, 350.0f));
         });
     }
 
-    private static void BuildAndOpenPopup()
+    private static void OpenPopup()
     {
+        // build colour config screen
         var changeGrabColourPage = MenuAPI.CreateREPOPopupPage("Change Grab Beam Colour", REPOPopupPage.PresetSide.Left, false, pageDimmerVisibility: true, spacing: 1.5f);
 
         changeGrabColourPage.AddElement(parent =>
         {
-            var redSlider = createColourSlider(
+            CreateBeamPreviewColourRectangle(parent);
+            SetBeamPreviewColourRectangleColour(CustomGrabBeamColour.LocalColour);
+
+            var redSlider = CreateColourSlider(
                 "Red",
                 "",
-                f => CustomGrabBeamColour.LocalColour.r = f,
+                f => {
+                    CustomGrabBeamColour.LocalColour.r = f;
+                    Color col = neutralGrabColourPreviewImage.color;
+                    col.r = f;
+                    neutralGrabColourPreviewImage.color = col;
+                    neutralGrabColourPreviewParentObject.gameObject.transform.localPosition = new Vector2(515f, 225f + f);
+                },
                 CustomGrabBeamColour.LocalColour.r,
                 parent,
                 3
             );
-            var greenSlider = createColourSlider(
+            var greenSlider = CreateColourSlider(
                 "Green",
                 "",
-                f => CustomGrabBeamColour.LocalColour.g = f,
+                f => {
+                    CustomGrabBeamColour.LocalColour.g = f;
+                    Color col = neutralGrabColourPreviewImage.color;
+                    col.g = f;
+                    neutralGrabColourPreviewImage.color = col;
+                },
                 CustomGrabBeamColour.LocalColour.g,
                 parent,
                 2
             );
-            var blueSlider = createColourSlider(
+            var blueSlider = CreateColourSlider(
                 "Blue",
                 "",
-                f => CustomGrabBeamColour.LocalColour.b = f,
+                f => {
+                    CustomGrabBeamColour.LocalColour.b = f;
+                    Color col = neutralGrabColourPreviewImage.color;
+                    col.b = f;
+                    neutralGrabColourPreviewImage.color = col;
+                },
                 CustomGrabBeamColour.LocalColour.b,
                 parent,
                 1
             );
-            var opacitySlider = createColourSlider(
+            var opacitySlider = CreateColourSlider(
                 "Opacity",
                 "",
-                f => CustomGrabBeamColour.LocalColour.a = f,
+                f => {
+                    CustomGrabBeamColour.LocalColour.a = f;
+                    Color col = neutralGrabColourPreviewImage.color;
+                    col.a = f;
+                    neutralGrabColourPreviewImage.color = col;
+                },
                 CustomGrabBeamColour.LocalColour.a,
                 parent,
                 0
             );
 
             var closeButton = MenuAPI.CreateREPOButton("Done", () => {
-                    changeGrabColourPage.ClosePage(true);
-                    CustomGrabBeamColour.UpdateBeamColour();
-                },
+                changeGrabColourPage.ClosePage(true);
+                CustomGrabBeamColour.UpdateBeamColour();
+            },
                 parent,
                 new Vector2(70f, 30f)
             );
@@ -73,7 +95,35 @@ class ConfigMenu
         changeGrabColourPage.OpenPage(false);
     }
 
-    public static REPOSlider createColourSlider(string name, string desc, Action<float> onChange, float initial, Transform parent, int offset)
+    private static void SetBeamPreviewColourRectangleColour(Color colour)
+    {
+        neutralGrabColourPreviewImage.color = colour;
+    }
+
+    private static void CreateBeamPreviewColourRectangle(Transform parent)
+    {
+        if(!neutralGrabColourPreviewParentObject)
+        {
+            neutralGrabColourPreviewParentObject = new GameObject();
+        }
+        neutralGrabColourPreviewParentObject.name = "Grab Beam Colour Preview Rectangle Parent";
+        neutralGrabColourPreviewParentObject.transform.SetParent(parent);
+
+        Canvas canvas = neutralGrabColourPreviewParentObject.AddComponent<Canvas>();
+        canvas.transform.SetParent(neutralGrabColourPreviewParentObject.gameObject.transform.parent);
+
+        GameObject imageGameObject = new GameObject();
+        imageGameObject.transform.SetParent(canvas.gameObject.transform.parent);
+
+        Image image = imageGameObject.AddComponent<Image>();
+        image.transform.SetParent(imageGameObject.gameObject.transform.parent);
+
+        image.color = new Color(1.0F, 0.0F, 0.0F);
+        neutralGrabColourPreviewImage = image;
+        neutralGrabColourPreviewImage.gameObject.transform.position = new Vector2(515f, 225f);
+    }
+
+    public static REPOSlider CreateColourSlider(string name, string desc, Action<float> onChange, float initial, Transform parent, int offset)
     {
         return MenuAPI.CreateREPOSlider(
             name,
