9e6ca1d562d549399eedefc7312bdab5e9289fb4
ioblackshaw <58823778+Enchanted-Games@users.noreply.github.com>
2025-03-31 23:29:40 +0100

    clean up config menu code a bit and add elements to scroll box

--- CustomGrabColour/Config/ConfigMenu.cs
+++ CustomGrabColour/Config/ConfigMenu.cs
@@ -9,11 +9,13 @@ using UnityEngine.UI;
 class ConfigMenu
 {
     private static Image neutralGrabColourPreviewImage;
-    private static GameObject neutralGrabColourPreviewParentObject;
+    private static GameObject neutralGrabColourPreviewParent;
+
+    private static float horizontalPos = 70f;
 
     public static void Init()
     {
-        MenuAPI.AddElementToEscapeMenu(parent =>
+        MenuAPI.AddElementToMainMenu(parent =>
         {
             // add button to open colour config screen
             var repoButton = MenuAPI.CreateREPOButton("Change Grab Colour", () => OpenPopup(), parent, localPosition: new Vector2(28.3f, 350.0f));
@@ -25,30 +27,50 @@ class ConfigMenu
         // build colour config screen
         var changeGrabColourPage = MenuAPI.CreateREPOPopupPage("Change Grab Beam Colour", REPOPopupPage.PresetSide.Left, false, pageDimmerVisibility: true, spacing: 1.5f);
 
+        // colour previews
         changeGrabColourPage.AddElement(parent =>
         {
-            CreateBeamPreviewColourRectangle(parent);
-            SetBeamPreviewColourRectangleColour(CustomGrabBeamColour.LocalNeutralColour.colour);
+            var disclaimerLabel = MenuAPI.CreateREPOLabel("Preview may not be 100% accurate ", parent, new Vector2(425f, 155f));
+            disclaimerLabel.transform.localScale = new Vector2(0.5f, 0.5f);
+            if(!neutralGrabColourPreviewParent)
+            {
+                CreateBeamPreviewColourRectangle(parent, new Vector2(515f, 225f), out neutralGrabColourPreviewParent, out neutralGrabColourPreviewImage);
+            }
+        });
 
+        // neutral grab beam colours
+        changeGrabColourPage.AddElementToScrollView(parent =>
+        {
+            var neutralBeamLabel = MenuAPI.CreateREPOLabel("Neutral Beam Colour", parent, new Vector2(0, getVerticalOffsetForScrollChildren(0)));
+            neutralBeamLabel.transform.localScale = new Vector2(0.5f, 0.5f);
+            return neutralBeamLabel.rectTransform;
+        });
+        changeGrabColourPage.AddElementToScrollView(parent =>
+        {
             var redSlider = CreateColourSlider(
                 "Red",
                 "",
-                f => {
+                f =>
+                {
                     CustomGrabBeamColour.LocalNeutralColour.r = f;
                     Color col = neutralGrabColourPreviewImage.color;
                     col.r = f;
                     neutralGrabColourPreviewImage.color = col;
-                    neutralGrabColourPreviewParentObject.gameObject.transform.localPosition = new Vector2(515f, 225f + f);
                 },
                 CustomGrabBeamColour.LocalNeutralColour.r,
                 1,
                 parent,
-                3
+                new Vector2(0, getVerticalOffsetForScrollChildren(1))
             );
+            return redSlider.rectTransform;
+        });
+        changeGrabColourPage.AddElementToScrollView(parent =>
+        {
             var greenSlider = CreateColourSlider(
                 "Green",
                 "",
-                f => {
+                f =>
+                {
                     CustomGrabBeamColour.LocalNeutralColour.g = f;
                     Color col = neutralGrabColourPreviewImage.color;
                     col.g = f;
@@ -57,12 +79,17 @@ class ConfigMenu
                 CustomGrabBeamColour.LocalNeutralColour.g,
                 1,
                 parent,
-                2
+                new Vector2(0, getVerticalOffsetForScrollChildren(2))
             );
+            return greenSlider.rectTransform;
+        });
+        changeGrabColourPage.AddElementToScrollView(parent =>
+        {
             var blueSlider = CreateColourSlider(
                 "Blue",
                 "",
-                f => {
+                f =>
+                {
                     CustomGrabBeamColour.LocalNeutralColour.b = f;
                     Color col = neutralGrabColourPreviewImage.color;
                     col.b = f;
@@ -71,12 +98,17 @@ class ConfigMenu
                 CustomGrabBeamColour.LocalNeutralColour.b,
                 1,
                 parent,
-                1
+                new Vector2(0, getVerticalOffsetForScrollChildren(3))
             );
+            return blueSlider.rectTransform;
+        });
+        changeGrabColourPage.AddElementToScrollView(parent =>
+        {
             var opacitySlider = CreateColourSlider(
                 "Opacity",
                 "",
-                f => {
+                f =>
+                {
                     CustomGrabBeamColour.LocalNeutralColour.a = f;
                     Color col = neutralGrabColourPreviewImage.color;
                     col.a = f;
@@ -85,28 +117,34 @@ class ConfigMenu
                 CustomGrabBeamColour.LocalNeutralColour.a,
                 CustomGrabColourConfig.MaxOpacity,
                 parent,
-                0
+                new Vector2(0, getVerticalOffsetForScrollChildren(4))
             );
+            return opacitySlider.rectTransform;
+        });
+        changeGrabColourPage.AddElementToScrollView(parent =>
+        {
             var matchSkinToggle = MenuAPI.CreateREPOToggle(
                 "Match Skin Colour",
                 (val) => { CustomGrabBeamColour.LocalNeutralColour.matchSkin = val; },
                 parent,
-                new Vector2(80f, 165.0f + (35 * -1)),
+                new Vector2(0, getVerticalOffsetForScrollChildren(5)),
                 "Yes",
                 "No",
                 CustomGrabBeamColour.LocalNeutralColour.matchSkin
             );
+            return matchSkinToggle.rectTransform;
+        });
 
-            var disclaimerLabel = MenuAPI.CreateREPOLabel("Preview may not be 100% accurate ", parent, new Vector2(425f, 155f));
-            disclaimerLabel.transform.localScale = new Vector2(0.5f, 0.5f);
-
+        // close and reset buttons
+        changeGrabColourPage.AddElement(parent =>
+        {
             var closeButton = MenuAPI.CreateREPOButton("Done", () => {
                 changeGrabColourPage.ClosePage(true);
                 CustomGrabBeamColour.UpdateBeamColourForAllBeams();
                 CustomGrabBeamColour.SaveLocalColoursToConfig();
             },
                 parent,
-                new Vector2(70f, 30f)
+                new Vector2(horizontalPos, 30f)
             );
 
             var resetButton = MenuAPI.CreateREPOButton("Reset", () => {
@@ -116,51 +154,50 @@ class ConfigMenu
                 CustomGrabBeamColour.SaveLocalColoursToConfig();
             },
                 parent,
-                new Vector2(290f, 30f)
+                new Vector2(590f, 30f)
             );
             resetButton.overrideButtonSize = resetButton.GetLabelSize() / 2;
             resetButton.transform.localScale = new Vector2(0.5f, 0.5f);
         });
 
+        // setup colour previews and open page
+        SetupPreviewRectangleColours(CustomGrabBeamColour.LocalNeutralColour.colour, CustomGrabBeamColour.LocalHealingColour.colour, CustomGrabBeamColour.LocalRotatingColour.colour);
         changeGrabColourPage.OpenPage(false);
     }
 
-    private static void SetBeamPreviewColourRectangleColour(Color colour)
+    private static void SetupPreviewRectangleColours(Color neutralColour, Color healingColour, Color rotatingColour)
     {
-        neutralGrabColourPreviewImage.color = colour;
+        neutralGrabColourPreviewImage.color = neutralColour;
     }
 
-    private static void CreateBeamPreviewColourRectangle(Transform parent)
+    private static void CreateBeamPreviewColourRectangle(Transform parent, Vector2 position, out GameObject colourPreviewParent, out Image colourPreviewImage)
     {
-        if(!neutralGrabColourPreviewParentObject)
-        {
-            neutralGrabColourPreviewParentObject = new GameObject();
-        }
-        neutralGrabColourPreviewParentObject.name = "Grab Beam Colour Preview Rectangle Parent";
-        neutralGrabColourPreviewParentObject.transform.SetParent(parent);
+        colourPreviewParent = new GameObject();
+        colourPreviewParent.name = "Grab Beam Colour Preview Rectangle Parent";
+        colourPreviewParent.transform.SetParent(parent);
 
-        Canvas canvas = neutralGrabColourPreviewParentObject.AddComponent<Canvas>();
-        canvas.transform.SetParent(neutralGrabColourPreviewParentObject.gameObject.transform.parent);
+        Canvas canvas = colourPreviewParent.AddComponent<Canvas>();
+        canvas.transform.SetParent(colourPreviewParent.gameObject.transform.parent);
 
         GameObject imageGameObject = new GameObject();
         imageGameObject.transform.SetParent(canvas.gameObject.transform.parent);
 
-        Image image = imageGameObject.AddComponent<Image>();
-        image.transform.SetParent(imageGameObject.gameObject.transform.parent);
+        colourPreviewImage = imageGameObject.AddComponent<Image>();
+        colourPreviewImage.name = "Grab Beam Colour Preview Rectangle Image";
+        colourPreviewImage.transform.SetParent(imageGameObject.gameObject.transform.parent);
 
-        image.color = new Color(1.0F, 0.0F, 0.0F);
-        neutralGrabColourPreviewImage = image;
-        neutralGrabColourPreviewImage.gameObject.transform.position = new Vector2(515f, 225f);
+        colourPreviewImage.color = new Color(1.0F, 0.0F, 0.0F);
+        colourPreviewImage.gameObject.transform.position = position;
     }
 
-    public static REPOSlider CreateColourSlider(string name, string desc, Action<float> onChange, float initial, float max, Transform parent, int offset)
+    public static REPOSlider CreateColourSlider(string name, string desc, Action<float> onChange, float initial, float max, Transform parent, Vector2 localPosition)
     {
         return MenuAPI.CreateREPOSlider(
             name,
             desc,
             onChange,
             parent,
-            new Vector2(80f, 165.0f + (35 * offset)),
+            localPosition,
             0,
             max,
             2,
@@ -169,5 +206,17 @@ class ConfigMenu
             "",
             REPOSlider.BarBehavior.UpdateWithValue
         );
+    }
+
+    private static float getVerticalOffsetForScrollChildren(int index)
+    {
+        return 40 * index;
+    }
+
+    private static float getVerticalPos(int offset)
+    {
+        int index = -Math.Abs(0 - offset);
+        index += 3;
+        return 180.0f + (30 * index);
     }
 }
